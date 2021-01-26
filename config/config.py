54408acc785e85ac1386189a4340cdea1a0f57ea
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-26 17:26:20 +0530

    Update and rename sample_config.py to config.py

--- config/config.py
+++ config/config.py
@@ -6,23 +6,23 @@ API_ID = 1234567
 API_HASH = "ab1c23def45fg67890h123i45678j9kl"
 
 # Get this from @Botfather
-TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+TOKEN = "1430367736:AAEXlPz5JSRBGaXzXilNM_EUFdJvJqw-gCg"
 
 # Your MongoDB URI (using a database named "vcpb"), if you don't provide, you can't use the playlist feature and wont see now playing message
 MONGO_DB_URI = ""
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
-    383407735,
-    951435494,
-    1392620345
+    1311769691,
+    1024689872,
+    1421068194
 ]
 
 # The ID of the group where your bot streams
-GROUP = -1001402753006
+GROUP = -1001291663564
 
 # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
-USERS_MUST_JOIN = False
+USERS_MUST_JOIN = true
 
 # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
 LANG = "en"
