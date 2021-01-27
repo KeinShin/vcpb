947618600f306b9bb82edda4af75454de30b382e
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-27 14:23:29 +0530

    Update pa.sh

--- pa.sh
+++ pa.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-#pa.sh
 
 pacmd load-module module-null-sink sink_name=MySink
 pactl load-module module-remap-source master=MySink.monitor source_name=virtual_microphone source_properties=device.description=VoiceChatPyroBot
